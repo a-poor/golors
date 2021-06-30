e38eb14d36a6b5d442fc639ab67e56c7cd47f864
a-poor <45295232+a-poor@users.noreply.github.com>
2021-06-30 10:29:53 -0700

    Adding some "tests"

--- golor_test.go
+++ golor_test.go
@@ -1,9 +1,40 @@
-package main
+package golor
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestPlaceholder(t *testing.T) {
 	if false {
 		t.Error("Hello world!")
 	}
+}
+
+func TestPrint(t *testing.T) {
+	fmt.Println("The following should all be on the same line...")
+	Red("red")
+	Green("green")
+	Yellow("yellow")
+	Blue("blue")
+	Purple("purple")
+	Cyan("cyan")
+	Gray("gray")
+	Grey("grey")
+	White("white")
+	fmt.Println("\nDone.")
+}
+
+func TestAllColors(t *testing.T) {
+	fmt.Println("The following should all be on different lines...")
+	Redln("This should be red")
+	Greenln("This should be green")
+	Yellowln("This should be yellow")
+	Blueln("This should be blue")
+	Purpleln("This should be purple")
+	Cyanln("This should be cyan")
+	Grayln("This should be gray")
+	Greyln("This should be grey")
+	Whiteln("This should be white")
+	fmt.Println("\nDone.")
 }
